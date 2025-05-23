673f9a917a5b8ba1ad43757a473a09686f80aef3
ViniLopes20 <vinicius.lopes@amigotech.com.br>
2025-05-23 03:30:45 -0300

    Adicionar arquivo de dados de teste do trabalho final

---
+++ dados-testes-trabalho-final.js
@@ -0,0 +1,31 @@
+export const dogsList = [
+  {
+    nome: 'Nick',
+    porte: 'pequeno',
+    peso: 10,
+    idade: 1
+  },
+  {
+    nome: 'thor',
+    porte: 'médio',
+    peso: 14.5,
+    idade: 2
+  },
+  {
+    nome: 'FLOQUINHO José de alencar',
+    porte: 'grande',
+    peso: 20,
+    idade: 3
+  },
+  {
+    nome: 'sombra',
+    porte: 'gigante',
+    peso: 30,
+    idade: 4
+  },
+  {
+    nome: 'buDwEisEr',
+    peso: 5,
+    idade: 5
+  }
+]
